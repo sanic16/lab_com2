a35d08367c6ee13b1cba31b3fab2c845738f461f
Julio Rubén Sanic Martínez <201222286@ingenieria.usac.edu.gt>
2022-02-17 20:00:26 -0600

    Cifrado hill completo

--- prac_1/cifrado_hill.py
+++ prac_1/cifrado_hill.py
@@ -1,3 +1,4 @@
+import numpy as np
 chars = {
     'A': 0,
     'B': 1,
@@ -13,55 +14,36 @@ chars = {
     'L': 11,
     'M': 12,
     'N': 13,
-    'Ñ': 14,
-    'O': 15,
-    'P': 16,
-    'Q': 17,
-    'R': 18,
-    'S': 19,
-    'T': 20,
-    'U': 21,
-    'V': 22,
-    'W': 23,
-    'X': 24,
-    'Y': 25,
-    'Z': 26,
-    'a': 27,
-    'b': 28,
-    'c': 29,
-    'd': 30,
-    'e': 31,
-    'f': 32,
-    'g': 33,
-    'h': 34,
-    'i': 35,
-    'j': 36,
-    'k': 37,
-    'l': 38,
-    'm': 39,
-    'n': 40,
-    'ñ': 41,
-    'o': 42,
-    'p': 43,
-    'q': 44,
-    'r': 45,
-    's': 46,
-    't': 47,
-    'u': 48,
-    'v': 49,
-    'w': 50,
-    'x': 51,
-    'y': 52,
-    'z': 53,
-    ' ': 54
+    'O': 14,
+    'P': 15,
+    'Q': 16,
+    'R': 17,
+    'S': 18,
+    'T': 19,
+    'U': 20,
+    'V': 21,
+    'W': 22,
+    'X': 23,
+    'Y': 24,
+    'Z': 25,
+    ' ' : 26
 }
 
-# initializing string
-test_str = 'Julio Ruben Sanic Martinez'
+# mensaje a enviar
+test_str = 'UNIVERSIDAD DE SAN CARLOS DE GUATEMALA'
+
+"""
+Se complementa con espacios si el mensaje no es divisible
+por 3
+"""
 extra = len(test_str)%3
 for x in range(3-extra):
     test_str += ' '
 
+"""
+Se crea una lista de Strings de 3 carácteres y usando
+mapeo se obtiene el entero equivalente
+"""
 i = 0
 my_list = list()
 while(i<len(test_str)):
@@ -69,10 +51,15 @@ while(i<len(test_str)):
     i+=3
 print(my_list)
 
+# Matrices 1-D 
 x = list()
 y = list()
 z = list()
 
+"""
+Se mapea de string a entero y se agregan los elementos
+a la matriz 1-D
+"""
 for in_list in my_list:
     i = 0
     for k in in_list:
@@ -86,3 +73,44 @@ for in_list in my_list:
 print(x)
 print(y)
 print(z)
+
+# Matríz llave 3x3, tiene que ser inversible
+key = [[35, 53, 12], [12, 21, 5], [2, 4, 1]]
+
+key = np.array(key)
+msg = np.array([x,y,z])
+# Multiplicando matríz mensaje por clave
+res = key@msg
+print(res)
+# Aplicando modulo len(chars)
+res = np.mod(res, len(chars))
+print(res)
+k = chars.keys()
+v = chars.values()
+d_chars = dict(zip(v,k))
+print(d_chars)
+
+
+x = list()
+y = list()
+z = list()
+
+i=0
+for in_list in res:
+    for k in in_list:
+        if i == 0:
+            x.append(k)
+        elif i == 1:
+            y.append(k)
+        elif i == 2:
+            z.append(k)
+    i+=1
+
+# Mensaje a enviar
+msg = ''
+i=0
+for j in range(len(x)):
+    msg += d_chars[x[j]] + d_chars[y[j]] + d_chars[z[j]]
+    i+=1
+print(msg)
+
